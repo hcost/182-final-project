38d8355de3c274bc2f13db7c0034dddf0d6f05f9
Harrison Costantino <harrisoncostantino@berkeley.edu>
2020-05-10 16:56:22 -0700

    cloud ready

--- baselines.py
+++ baselines.py
@@ -13,25 +13,30 @@ root_dir = os.getcwd()
 model_name = root_dir + '/models/'+ 'pretrained_ppo2'
 use_vec_envs = True
 number_of_vec_envs = 4
-framestack = 3 #NOTE: cannot use framestacking and pretraining together
+framestack = 0 #NOTE: cannot use framestacking and pretraining together
 use_pretraining = True
 expert_dir = root_dir + '/' + 'i_am_the_expert.npz'
 traj_limitation = -1 #-1 uses whole dataset
 pretraining_epochs = 10
-training_iterations = 1000
+training_iterations = 10000
 policy = 'CnnPolicy'
 seed = 0
 log_dir = root_dir + '/' + 'stats/pretrain_ppo2'
 vid_dir = root_dir + '/' + 'videos/pretrain_ppo2'
 vid_freq = 250
+vid_length = 400
 verbose = 1
 num_levels = 100
+seq_levels = True
+record = True
+normalize = True
 
+#Note: Cannot record after loading!
 
 print("Initializing")
 def make_env(rank, seed=seed):
 	def func():
-		env = gym.make(id='procgen:procgen-fruitbot-v0', num_levels=num_levels, distribution_mode='easy')
+		env = gym.make(id='procgen:procgen-fruitbot-v0', use_sequential_levels=seq_levels, num_levels=num_levels, distribution_mode='easy')
 		env.seed(rank+seed)
 		return env
 	set_global_seeds(seed)
@@ -47,17 +52,20 @@ def make_vec(num):
 
 if use_vec_envs:
 	env = make_vec(number_of_vec_envs)
-	env = sbc.vec_env.VecNormalize(env)
-	# env = sbc.vec_env.VecFrameStack(env, framestack)
-	env = sbc.vec_env.VecVideoRecorder(env, vid_dir, vid_trigger)
+	if normalize:
+		env = sbc.vec_env.VecNormalize(env)
+	if framestack:
+		env = sbc.vec_env.VecFrameStack(env, framestack)
+	if record:
+		env = sbc.vec_env.VecVideoRecorder(env, vid_dir, vid_trigger, video_length=vid_length)
 else:
-	env = gym.make(id='procgen:procgen-fruitbot-v0', num_levels=num_levels, distribution_mode='easy')
+	env = gym.make(id='procgen:procgen-fruitbot-v0', use_sequential_levels=seq_levels, num_levels=num_levels, distribution_mode='easy')
 
 
 model = sb.PPO2(policy, env, verbose=verbose, tensorboard_log=log_dir)
 
 if use_pretraining:
-	print("Pretraining")
+	print("Pretraining\n")
 	dataset = ExpertDataset(expert_path=expert_dir, traj_limitation=traj_limitation, verbose=verbose)
 	model.pretrain(dataset, n_epochs=pretraining_epochs)
 
@@ -66,3 +74,4 @@ model.learn(training_iterations)
 
 
 model.save(model_name)
+env.close()
